60790952a1384e0a57095effd21a60ea7e2d92e3
dhananjay <dhananjay.s@aveosoft.com>
2022-11-11 17:48:58 +0530

    bug fix

--- lib/aveonotification.dart
+++ lib/aveonotification.dart
@@ -6,5 +6,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:d2d_notifications/d2d_notifications.dart';
 
+export 'package:firebase_messaging/firebase_messaging.dart';
+
 part 'src/fcm_notification.dart';
 part 'src/local_notification.dart';
